323b6a31426e5ccd6c3f0c07d2e9c254e291156e
Bijo <bijo.panicker@cuemath.com>
2022-12-14 12:08:46 +0530

    changed code

--- server.mjs
+++ server.mjs
@@ -1,82 +1,99 @@
-import express from 'express'
-const app = express()
-const port = process.env.PORT || 3000
+import { MongoClient } from 'mongodb'
 
-// app.get('/', (req, res) => {
-//   console.log('Response from bijo' + req.ip);
-//   res.send('Response from bijo' + req.ip);
+MongoClient.connect('mongodb+srv://bijo:TfE68elk91rxn7zV@cluster0.gbjr68m.mongodb.net/gamedata', (err, client) => {
+  if (err) throw err
+
+  const db = client.db('gamedata')
+
+  db.collection('gamedata').find().toArray((err, result) => {
+    if (err) throw err
+
+    console.log(result)
+  })
+})
+
+
+
+
+// import express from 'express'
+// const app = express()
+// const port = process.env.PORT || 3000
+
+// // app.get('/', (req, res) => {
+// //   console.log('Response from bijo' + req.ip);
+// //   res.send('Response from bijo' + req.ip);
+// // })
+
+// // const mongoose = require("mongoose");
+
+// import mongoose from 'mongoose'
+
+
+// app.use(express.json());
+
+// // app.use(cors({origin:true}));
+
+// mongoose.connect("mongodb+srv://bijo:TfE68elk91rxn7zV@cluster0.gbjr68m.mongodb.net/gamedata",{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// },(err)=>{
+
+//     if(!err){
+//         console.log("Conected to db");
+//         res.send("Conected to db");
+
+//     }else{
+//         console.log("error is there");
+//         console.log(err);
+//         res.send("error is there");
+
+
+//     }
+
 // })
 
-// const mongoose = require("mongoose");
 
-import mongoose from 'mongoose'
-
-
-app.use(express.json());
-
-// app.use(cors({origin:true}));
-
-mongoose.connect("mongodb+srv://bijo:TfE68elk91rxn7zV@cluster0.gbjr68m.mongodb.net/gamedata",{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-},(err)=>{
-
-    if(!err){
-        console.log("Conected to db");
-        res.send("Conected to db");
-
-    }else{
-        console.log("error is there");
-        console.log(err);
-        res.send("error is there");
+// //schema
+// // const sch = {
+// //     name:String,
+// //     email:String,
+// //     id:Number
+// // }
+// // const monmodel = mongoose.model("NEWCOL",sch);
 
 
-    }
+// //post
+// app.post("/post",async(req,res)=>{
+//     console.log("inside post function");
+//     // const data = new monmodel({
+//     //     name:"testnameiadded",
+//     //     email:"testemailiadded",
+//     //     id:4214525
+//     // });
 
-})
+//     const data = {
+//         name:"testnameiadded",
+//         email:"testemailiadded",
+//         id:4214525
+//     };
 
+//     const val = await data.save();
 
-//schema
-// const sch = {
-//     name:String,
-//     email:String,
-//     id:Number
-// }
-// const monmodel = mongoose.model("NEWCOL",sch);
+//     res.json(val); 
+//     // res.json({
+//     //     name:"testnameiadded",
+//     //     email:"testemailiadded",
+//     //     id:4214525
+//     // }); 
 
-
-//post
-app.post("/post",async(req,res)=>{
-    console.log("inside post function");
-    // const data = new monmodel({
-    //     name:"testnameiadded",
-    //     email:"testemailiadded",
-    //     id:4214525
-    // });
-
-    const data = {
-        name:"testnameiadded",
-        email:"testemailiadded",
-        id:4214525
-    };
-
-    const val = await data.save();
-
-    res.json(val); 
-    // res.json({
-    //     name:"testnameiadded",
-    //     email:"testemailiadded",
-    //     id:4214525
-    // }); 
-
-    res.send("posted"); 
-})
+//     res.send("posted"); 
+// })
 
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
 
-  res.send(`Example app listening on port ${port}`); 
+//   res.send(`Example app listening on port ${port}`); 
 
-})
+// })
