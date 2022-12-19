ddb98b580925e944c38dc0398764e77cc9ea2686
Bijo <bijo.panicker@cuemath.com>
2022-12-19 12:43:16 +0530

    commented express js

--- server.mjs
+++ server.mjs
@@ -6,8 +6,8 @@ const port = process.env.PORT || 3000
 
 import mongoose from 'mongoose'
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }))
 
 ///
 
